78e9dd1f70b01c30e140c84958306539690906f9
Yash <yashkd12790@gmail.com>
2024-06-01 23:17:19 +0530

    worked on profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -7,6 +7,9 @@ import { useRouter } from "next/navigation";
 
 export default function ProfilePage() {
   const router = useRouter();
+
+  const [data, setData] = useState("nothing");
+
   const logout = async () => {
     try {
       await axios.get("/api/users/logout");
@@ -18,17 +21,36 @@ export default function ProfilePage() {
     }
   };
 
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p>Profile Page</p>
       <hr />
+      <h2 className="p-1 rounded bg-green-500">
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <button
         onClick={logout}
         className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        GetUser Details
       </button>
     </div>
   );
